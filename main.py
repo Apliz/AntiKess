e5c5bd5b104417a5f399906c3d1526acedb795b6
Apliz <antonplisnier@gmail.com>
2024-02-03 16:48:43 +0000

    made orbital data accessible to matplotlib

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
 import matplotlib.pyplot as plt
+import json
 
+f = open("./lib/JSON/orbital_data.json")
+orbitalJSON = json.load(f)
+
+print(orbitalJSON[0]["OBJECT_NAME"])
+
+# for i in orbitalJSON[0]:
+#     print(i)
+ 
+# Closing file
+f.close()
 plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
 plt.show()
