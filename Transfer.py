71f75696d96ca5b80f45bc71f4ef38db06513ffa
Apliz <antonplisnier@gmail.com>
2024-03-29 16:31:17 +0000

    Removed apsis() & refactored hohamnn_transfer()

--- Transfer.py
+++ Transfer.py
@@ -18,27 +18,21 @@ class Transfer(Orbit):
 
         v1 = self.departure_orbit.velocity_at_position("apogee")
         v2 = self.arrival_orbit.velocity_at_position("perigee")
-        
-        transfer_ap = None
-        transfer_pe = None
 
-        transfer_start_point_radius = self.departure_orbit.radius_at("apogee")
-        transfer_end_point_radius = self.arrival_orbit.radius_at("perigee")
-    
-        if transfer_end_point_radius > transfer_start_point_radius:
-            transfer_ap, transfer_pe = Transfer.switch_apsis(transfer_start_point_radius, transfer_end_point_radius)
-
-        self.transfer_orbit = Transfer.orbit(
-            transfer_ap,
-            transfer_pe
-        )
-
-        if self.transfer_orbit.radius_at("apogee") == transfer_start_point_radius:
-            vt1 = self.transfer_orbit.velocity_at_position("apogee")
-            vt2 = self.transfer_orbit.velocity_at_position("perigee")
-        if self.transfer_orbit.radius_at("apogee") != transfer_start_point_radius:
+        if v1 > v2:
+            self.transfer_orbit = Transfer.orbit(
+                self.arrival_orbit.radius_at("perigee"),
+                self.departure_orbit.radius_at("apogee")
+            )
             vt1 = self.transfer_orbit.velocity_at_position("perigee")
             vt2 = self.transfer_orbit.velocity_at_position("apogee")
+        elif v1 < v2:
+            self.transfer_orbit = Transfer.orbit(
+                self.departure_orbit.radius_at("apogee"),
+                self.arrival_orbit.radius_at("perigee")
+            )
+            vt1 = self.transfer_orbit.velocity_at_position("apogee")
+            vt2 = self.transfer_orbit.velocity_at_position("perigee")
 
         delta_v1 = vt1 - v1
         delta_v2 = vt2 - v2
@@ -53,15 +47,8 @@ class Transfer(Orbit):
         return ap, pe
     
     @staticmethod
-    def apsis(r1, r2):
-        ap = r2
-        pe = r1
-        return ap, pe
-    
-    @staticmethod
     def orbit(ap,pe):
         e = Orbit.eccentricity(ap, pe)
         a = Orbit.a(ap, pe)
         P = Orbit.period(a)
-        n = Orbit.mean_motion(a)
-        return Orbit(n,e,ap,pe,P,a)
+        return Orbit(e,P,a)
