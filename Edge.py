c6c6ce8a6b277caf9c7dd1252df86ae48e2049ea
Apliz <antonplisnier@gmail.com>
2024-04-01 17:35:23 +0100

    created skeleton of edge object

---
+++ Edge.py
@@ -0,0 +1,19 @@
+"Module docstring to be composed"
+
+class Edge():
+    def __init__(self,start_vertex, end_vertex, weight):
+        self.start = start_vertex
+        self.end = end_vertex
+        self.weight = weight
+    
+    def get_start(self):
+        "Method docstring to be composed"
+        return self.start
+
+    def get_end(self):
+        "Method docstring to be composed"
+        return self.end
+    
+    def get_weight(self):
+        "Method docstring to be composed"
+        return self.weight
