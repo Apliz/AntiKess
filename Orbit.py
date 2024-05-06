9e22f31cf8436486e8a0d11c853d9406594e66ab
Apliz <antonplisnier@gmail.com>
2024-05-06 18:53:02 +0100

    introduced first verion of orbital inclination deltaV calculation

--- Orbit.py
+++ Orbit.py
@@ -1,5 +1,5 @@
 """Orbit Class"""
-from math import pi, sqrt
+from math import pi, sqrt, sin, cos, acos
 from constants import GM
 
 class Orbit():
@@ -9,10 +9,14 @@ class Orbit():
     `semi_major_axis` -> distance in `km` \n
     
     """
-    def __init__(self, eccentricity, period, semi_major_axis):
+    def __init__(self, eccentricity, period, semi_major_axis, mean_anomaly, inclination, mean_motion, arg_of_pericenter):
         self.eccentricity = eccentricity
         self.period = period
         self.semi_major_axis = semi_major_axis
+        self.mean_anomaly = mean_anomaly
+        self.inclination = inclination
+        self.mean_motion = mean_motion
+        self.arg_of_pericenter = arg_of_pericenter
 
     def radius_at(self, position:str) -> int:
         """ Returns the orbital radius at a given position in `km`\n`
@@ -44,6 +48,23 @@ class Orbit():
         """Returns the period of an orbit `s`"""
         t = 2*pi*sqrt(self.semi_major_axis**3 / GM)
         return t
+
+    def true_anomaly(self):
+        """calculates true_anomaly of an orbit in"""
+
+        i = self.mean_anomaly * (pi/180)
+        i_next = 1
+
+        while ((i_next - i)/i_next) > 0.01:
+            i_next = i * (self.eccentricity * sin(i))
+            i = i_next
+
+        true_anomaly = acos( (cos(i_next) - self.eccentricity) / 1 - (self.eccentricity * cos(i_next)  ) )
+
+        return true_anomaly
+        # https://blog.hardinglabs.com/tle-to-kep.html
+
+
 
     @staticmethod
     def period(a):
--- Transfer.py
+++ Transfer.py
@@ -1,5 +1,5 @@
 """Transfer Class"""
-from math import sqrt
+from math import sqrt, sin, cos
 from constants import GM
 from Orbit import Orbit
 
@@ -12,7 +12,7 @@ class Transfer(Orbit):
         self.departure_orbit = departure_orbit
         self.arrival_orbit = arrival_orbit
         self.transfer_orbit = None
-    
+
     def hohmann_transfer(self):
         """Returns total deltaV for a hohmann transfer manoeuvre in `km s-1`"""
 
@@ -40,6 +40,28 @@ class Transfer(Orbit):
         total_delta_v = abs(delta_v1) + abs(delta_v2)
         return f'total delta v for transfer is: {total_delta_v} km s-1'
 
+
+    # Continue working from here!
+    # The core calculation is correct, but for example:
+    # If the correct answer is 337.0 m/s DeltaV to complete a inclination change
+    # The method will instead output 3370 m/s DeltaV
+
+    # Next steps are to find the weird maths happening.
+    def inclination_change(self):
+        """Returns the inclination in `degrees`"""
+
+        # Surely not...
+        e = self.departure_orbit.eccentricity
+        f = self.departure_orbit.true_anomaly()
+        n = self.departure_orbit.mean_motion
+        a = self.departure_orbit.semi_major_axis
+        w = self.departure_orbit.arg_of_pericenter
+
+        delta_i = abs(self.departure_orbit.inclination - self.arrival_orbit.inclination)
+        deltaV = (2 * sin(delta_i / 2) * (1 + (e * cos(f))*n*a )) / (sqrt(1 - e**2)*cos(w + f))
+
+        return deltaV
+            
     @staticmethod
     def switch_apsis(ap_in,pe_in):
         ap = pe_in
